5ac1d178c7b4e0329c10df3a8981177002b64328
HUAWEI <sanchous2810@gmail.com>
2022-03-13 01:44:30 +0300

    huawei eighth commit

--- app/src/main/java/com/example/hakaton2/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/hakaton2/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.hakaton2.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,8 +29,10 @@ public class DashboardFragment extends Fragment {
     public Button roll_bt;
     public Button roll_info_bt;
     public TextView roll_text;
-    public Points points = new Points();
+    public TextView points_title;
+    public static Points points = new Points();
     public Roulette roulette = new Roulette();
+    public static boolean vis_roll = true;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,23 +42,117 @@ public class DashboardFragment extends Fragment {
         roll_bt = (Button) root.findViewById(R.id.roll_bt);
         roll_info_bt = (Button) root.findViewById(R.id.roll_info_bt);
         roll_text = (TextView) root.findViewById(R.id.roll_text);
+        points_title = (TextView) root.findViewById(R.id.points_title);
+        points_title.setText( points.getPoints() + " ИКТИБаллов");
+        if (points.getPoints() < 5){
+            vis_roll = false;
+        }
+        else {
+            vis_roll = true;
+        }
+        roll_bt.setEnabled(vis_roll);
+
 
         roll_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                points.rouletteRoll();
-                roulette.rouletteMechanism();
-                for (int x = 0; x < 100; x++){
-                    random_value = (int) (Math.random()*99);
+
+                if (points.getPoints() < 5) {
+                    vis_roll = false;
+                    roll_bt.setEnabled(vis_roll);
+                    Toast.makeText(getContext(), "Недостаточно ИКТИБаллов", Toast.LENGTH_LONG).show();
+                } else {
+                    points.rouletteRoll();
+                    points_title.setText(points.getPoints() + " ИКТИБаллов");
+                    roulette.rouletteMechanism();
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         @Override
                         public void run() {
+                            random_value = (int) (Math.random() * 99);
                             roll_text.setText(gifts[random_value]);
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    random_value = (int) (Math.random() * 99);
+                                    roll_text.setText(gifts[random_value]);
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            random_value = (int) (Math.random() * 99);
+                                            roll_text.setText(gifts[random_value]);
+                                            handler.postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    random_value = (int) (Math.random() * 99);
+                                                    roll_text.setText(gifts[random_value]);
+                                                    handler.postDelayed(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            random_value = (int) (Math.random() * 99);
+                                                            roll_text.setText(gifts[random_value]);
+                                                            handler.postDelayed(new Runnable() {
+                                                                @Override
+                                                                public void run() {
+                                                                    random_value = (int) (Math.random() * 99);
+                                                                    roll_text.setText(gifts[random_value]);
+                                                                    handler.postDelayed(new Runnable() {
+                                                                        @Override
+                                                                        public void run() {
+                                                                            random_value = (int) (Math.random() * 99);
+                                                                            roll_text.setText(gifts[random_value]);
+                                                                            handler.postDelayed(new Runnable() {
+                                                                                @Override
+                                                                                public void run() {
+                                                                                    random_value = (int) (Math.random() * 99);
+                                                                                    roll_text.setText(gifts[random_value]);
+                                                                                    handler.postDelayed(new Runnable() {
+                                                                                        @Override
+                                                                                        public void run() {
+                                                                                            random_value = (int) (Math.random() * 99);
+                                                                                            roll_text.setText(gifts[random_value]);
+                                                                                            handler.postDelayed(new Runnable() {
+                                                                                                @Override
+                                                                                                public void run() {
+                                                                                                    random_value = (int) (Math.random() * 99);
+                                                                                                    roll_text.setText(gifts[random_value]);
+                                                                                                    handler.postDelayed(new Runnable() {
+                                                                                                        @Override
+                                                                                                        public void run() {
+                                                                                                            random_value = (int) (Math.random() * 99);
+                                                                                                            roll_text.setText(gifts[random_value]);
+                                                                                                            handler.postDelayed(new Runnable() {
+                                                                                                                @Override
+                                                                                                                public void run() {
+                                                                                                                    random_value = (int) (Math.random() * 99);
+                                                                                                                    roll_text.setText(gifts[random_value]);
+                                                                                                                    roll_text.setTextColor(Color.BLACK);
+                                                                                                                }
+                                                                                                            }, 1000);
+                                                                                                        }
+                                                                                                    }, 900);
+                                                                                                }
+                                                                                            }, 800);
+                                                                                        }
+                                                                                    }, 750);
+                                                                                }
+                                                                            }, 600);
+                                                                        }
+                                                                    }, 600);
+                                                                }
+                                                            }, 600);
+                                                        }
+                                                    }, 500);
+                                                }
+                                            }, 400);
+                                        }
+                                    }, 400);
+                                }
+                            }, 300);
                         }
-                    }, 2000);
-                }
+                    }, 300);
 
+                }
             }
         });
 
