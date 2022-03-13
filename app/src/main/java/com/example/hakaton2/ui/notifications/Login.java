bd09f7c3c49faa623521c6555687bd9a2ec6c4d7
kiril <kirillkravcenko029@gmail.com>
2022-03-13 11:53:25 +0300

    commot 11.52

--- app/src/main/java/com/example/hakaton2/ui/notifications/Login.java
+++ app/src/main/java/com/example/hakaton2/ui/notifications/Login.java
@@ -2,15 +2,30 @@ package com.example.hakaton2.ui.notifications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.hakaton2.R;
 
 public class Login extends AppCompatActivity {
 
+    public Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn = (Button) findViewById(R.id.btnLog);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Kabinet.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hakaton2/ui/notifications/Register.java
+++ app/src/main/java/com/example/hakaton2/ui/notifications/Register.java
@@ -2,15 +2,30 @@ package com.example.hakaton2.ui.notifications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.hakaton2.R;
 
 public class Register extends AppCompatActivity {
 
+    public Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btn = (Button) findViewById(R.id.btnReg);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Kabinet.class);
+                startActivity(intent);
+            }
+        });
     }
 }
